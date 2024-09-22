302b65124fb9e9fd04079ff6727b5f2c12f810b7
Keshav <keshav7aug@gmail.com>
2024-09-23 01:31:35 +0530

    changes to animation

--- MultiWindowClock/clock.py
+++ MultiWindowClock/clock.py
@@ -18,10 +18,12 @@ class Colors:
     yellow = (255,255,0,255)
 
 def getCurrentTime(numberOfMonitors):
-    now = datetime.now().strftime("%H:%M:%S:%x")
+    now = datetime.now().strftime("%H:%M:%S")
+    theTime = now
     now = now.split(":")
-    if numberOfMonitors == 1:
-        now = [f"{now[0]} : {now[1]}"]
+    now.append(theTime)
+    # if numberOfMonitors == 1:
+    #     now = [f"{now[0]} : {now[1]}"]
     return now
 
 def getProgress(val,fSH,monitorN):
@@ -41,7 +43,7 @@ def getProgress(val,fSH,monitorN):
         divisor = divisiors[monitorN]*1000
         prevTimeUnit = divisor*(math.floor(currentTime/divisor))
         timeElapsed =  currentTime - prevTimeUnit
-        return (timeElapsed/divisor)*fSH
+        return (timeElapsed/divisor)*(fSH)
     return 0   
 
 def getNextTime(monitorN):
@@ -51,7 +53,7 @@ def getNextTime(monitorN):
     elif monitorN == 1:
         val = (currentTime+timedelta(minutes=1)).minute
     elif monitorN == 2:
-        val = (currentTime+timedelta(seconds==1)).second
+        val = (currentTime+timedelta(seconds=1)).second
     if val<10:
         return f"0{val}"
     return f"{val}"
@@ -70,10 +72,10 @@ def getFont(nextVal,i,timeText,sw,sh,selectedFont,renderer):
     fontSize = int(min(sw*r1, sh*r2))
     if nextVal == True:
         text = f"{timeText}"
-        text_color = Colors.white
+        text_color = Colors.grey
     else:
         text = timeText[i]
-        text_color = Colors.grey
+        text_color = Colors.red
     fontSize = (2*fontSize)//(len(text.replace(" ","")) - text.count(":"))
     print(nextVal,fontSize,text)
     font = pygame.font.SysFont(selectedFont, fontSize)
@@ -94,7 +96,7 @@ def getAnimationArgs(val,fSH, monitorN):
         jitter_y = jitterPerc
     return jitter_x, jitter_y
 
-animation = "odometer"
+animation = "no"
 def run_screensaver():
     pygame.init()
     background_color = (0, 0, 0)
@@ -114,30 +116,38 @@ def run_screensaver():
                 running = False
         currentTime = getCurrentTime(numberOfMonitors)
         
-        for i,dispInfo in enumerate(renderers):
-            renderer, window = dispInfo
+        # for i,dispInfo in enumerate(renderers):
+        for j,i in enumerate(Orientation):
+            renderer, window = renderers[j]
             sw, sh = window.size
             renderer.clear()
             renderer.draw_color = Colors.black
-            nextTime = getNextTime(i)
+            if animation == "odometer":
+                nextTime = getNextTime(i)
             fSH, text_texture = getFont(False,i,currentTime,sw,sh,selectedFont,renderer)
-            _,text_texture_next = getFont(True,i,nextTime,sw,sh,selectedFont,renderer)
-            totalHeight = (sh)/2
+            if animation == "odometer":
+                _,text_texture_next = getFont(True,i,nextTime,sw,sh,selectedFont,renderer)
+            totalHeight = (sh+(fSH/2))/2
             jitter_x, jitter_y = getAnimationArgs(val,totalHeight,i)
             if animation == "jitter":
                 pos1,pos2 = (sw//2)+jitter_x, (sh//2)+jitter_y
             elif animation == "odometer":
+                # jitter_y=0
                 pos1,pos2 = (sw//2), (sh//2)+jitter_y
                 # print(pos1,pos2,jitter_y,jitterPerc)
             else:
                 pos1,pos2 = (sw//2), (sh//2)
-            text_rect = text_texture.get_rect(center = (pos1,pos2))
-            text_rect_next = text_texture_next.get_rect(center = (pos1,jitter_y))
+            text_rect = text_texture.get_rect(centerx = pos1, centery=pos2)
+            if animation == "odometer":
+                text_rect_next = text_texture_next.get_rect(centerx=pos1,centery=pos2-((sh+fSH)/2))
             renderer.blit(text_texture, text_rect)
-            renderer.blit(text_texture_next, text_rect_next)
+            if animation == "odometer":
+                renderer.blit(text_texture_next, text_rect_next)
             renderer.present()
         clock.tick(60)
         val *= -1
     pygame.quit()
+
+Orientation = [3]
 run_screensaver()
                 
